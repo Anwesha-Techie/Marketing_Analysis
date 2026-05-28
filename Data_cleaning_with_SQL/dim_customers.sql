c15169bd8e160f81bda7f785d734392b06e24d39
Anwesha <30508482+Anwesha-Techie@users.noreply.github.com>
2026-05-28 16:13:56 +0530

    Add files via upload

---
+++ Data_cleaning_with_SQL/dim_customers.sql
@@ -0,0 +1,22 @@
+SELECT *
+FROM dbo.customers;
+
+SELECT *
+FROM dbo.geography;
+
+-- SQL statement to join dim_customers with dim_geography to enrich customer data with geographic information
+
+SELECT 
+	c.CustomerID,
+	c.CustomerName,
+	c.Email,
+	c.Gender,
+	c.Age,
+	g.City,
+	g.Country
+FROM 
+	dbo.customers c
+LEFT JOIN
+	dbo.geography g
+ON
+	c.GeographyID = g.GeographyID;
